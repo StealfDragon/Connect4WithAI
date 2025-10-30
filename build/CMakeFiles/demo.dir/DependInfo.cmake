58524c97a55cc8a883a9204deef1798c53d724f3
Cassian Jones <cassian.jones@icloud.com>
2025-10-30 13:43:02 -0700

    Initial commit - Cassian

---
+++ build/CMakeFiles/demo.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/Cassian/School_Local/CMPM_123/Week5/Connect4WithAI/Application.cpp" "CMakeFiles/demo.dir/Application.cpp.o" "gcc" "CMakeFiles/demo.dir/Application.cpp.o.d"
+  "/Users/Cassian/School_Local/CMPM_123/Week5/Connect4WithAI/classes/Bit.cpp" "CMakeFiles/demo.dir/classes/Bit.cpp.o" "gcc" "CMakeFiles/demo.dir/classes/Bit.cpp.o.d"
+  "/Users/Cassian/School_Local/CMPM_123/Week5/Connect4WithAI/classes/BitHolder.cpp" "CMakeFiles/demo.dir/classes/BitHolder.cpp.o" "gcc" "CMakeFiles/demo.dir/classes/BitHolder.cpp.o.d"
+  "/Users/Cassian/School_Local/CMPM_123/Week5/Connect4WithAI/classes/Checkers.cpp" "CMakeFiles/demo.dir/classes/Checkers.cpp.o" "gcc" "CMakeFiles/demo.dir/classes/Checkers.cpp.o.d"
+  "/Users/Cassian/School_Local/CMPM_123/Week5/Connect4WithAI/classes/ChessSquare.cpp" "CMakeFiles/demo.dir/classes/ChessSquare.cpp.o" "gcc" "CMakeFiles/demo.dir/classes/ChessSquare.cpp.o.d"
+  "/Users/Cassian/School_Local/CMPM_123/Week5/Connect4WithAI/classes/Game.cpp" "CMakeFiles/demo.dir/classes/Game.cpp.o" "gcc" "CMakeFiles/demo.dir/classes/Game.cpp.o.d"
+  "/Users/Cassian/School_Local/CMPM_123/Week5/Connect4WithAI/classes/Grid.cpp" "CMakeFiles/demo.dir/classes/Grid.cpp.o" "gcc" "CMakeFiles/demo.dir/classes/Grid.cpp.o.d"
+  "/Users/Cassian/School_Local/CMPM_123/Week5/Connect4WithAI/classes/Othello.cpp" "CMakeFiles/demo.dir/classes/Othello.cpp.o" "gcc" "CMakeFiles/demo.dir/classes/Othello.cpp.o.d"
+  "/Users/Cassian/School_Local/CMPM_123/Week5/Connect4WithAI/classes/Sprite.cpp" "CMakeFiles/demo.dir/classes/Sprite.cpp.o" "gcc" "CMakeFiles/demo.dir/classes/Sprite.cpp.o.d"
+  "/Users/Cassian/School_Local/CMPM_123/Week5/Connect4WithAI/classes/Square.cpp" "CMakeFiles/demo.dir/classes/Square.cpp.o" "gcc" "CMakeFiles/demo.dir/classes/Square.cpp.o.d"
+  "/Users/Cassian/School_Local/CMPM_123/Week5/Connect4WithAI/classes/TicTacToe.cpp" "CMakeFiles/demo.dir/classes/TicTacToe.cpp.o" "gcc" "CMakeFiles/demo.dir/classes/TicTacToe.cpp.o.d"
+  "/Users/Cassian/School_Local/CMPM_123/Week5/Connect4WithAI/imgui/imgui.cpp" "CMakeFiles/demo.dir/imgui/imgui.cpp.o" "gcc" "CMakeFiles/demo.dir/imgui/imgui.cpp.o.d"
+  "/Users/Cassian/School_Local/CMPM_123/Week5/Connect4WithAI/imgui/imgui_demo.cpp" "CMakeFiles/demo.dir/imgui/imgui_demo.cpp.o" "gcc" "CMakeFiles/demo.dir/imgui/imgui_demo.cpp.o.d"
+  "/Users/Cassian/School_Local/CMPM_123/Week5/Connect4WithAI/imgui/imgui_draw.cpp" "CMakeFiles/demo.dir/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/demo.dir/imgui/imgui_draw.cpp.o.d"
+  "/Users/Cassian/School_Local/CMPM_123/Week5/Connect4WithAI/imgui/imgui_impl_glfw.cpp" "CMakeFiles/demo.dir/imgui/imgui_impl_glfw.cpp.o" "gcc" "CMakeFiles/demo.dir/imgui/imgui_impl_glfw.cpp.o.d"
+  "/Users/Cassian/School_Local/CMPM_123/Week5/Connect4WithAI/imgui/imgui_impl_opengl3.cpp" "CMakeFiles/demo.dir/imgui/imgui_impl_opengl3.cpp.o" "gcc" "CMakeFiles/demo.dir/imgui/imgui_impl_opengl3.cpp.o.d"
+  "/Users/Cassian/School_Local/CMPM_123/Week5/Connect4WithAI/imgui/imgui_tables.cpp" "CMakeFiles/demo.dir/imgui/imgui_tables.cpp.o" "gcc" "CMakeFiles/demo.dir/imgui/imgui_tables.cpp.o.d"
+  "/Users/Cassian/School_Local/CMPM_123/Week5/Connect4WithAI/imgui/imgui_widgets.cpp" "CMakeFiles/demo.dir/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/demo.dir/imgui/imgui_widgets.cpp.o.d"
+  "/Users/Cassian/School_Local/CMPM_123/Week5/Connect4WithAI/main_macos.cpp" "CMakeFiles/demo.dir/main_macos.cpp.o" "gcc" "CMakeFiles/demo.dir/main_macos.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/Cassian/School_Local/CMPM_123/Week5/Connect4WithAI
+# Build directory: /Users/Cassian/School_Local/CMPM_123/Week5/Connect4WithAI/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
